939a5884cab49fb22aad015ec0b8765ba0c44186
gozal1 <gadayevagozla@gmail.com>
2022-12-22 17:05:30 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import QRCode from './QRCode/qrCode';
+
+
+function App() {
+  return (
+    <div className="App">
+      <QRCode/>
+    </div>
+  );
+}
+
+export default App;
